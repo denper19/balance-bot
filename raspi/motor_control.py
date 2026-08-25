3c726abf9f3444e10d9a154dde4cf635e897da0f
jermito <jermito24@gmail.com>
2026-08-25 00:41:50 -0700

    counts encoder ticks

--- raspi/motor_control.py
+++ raspi/motor_control.py
@@ -1,5 +1,7 @@
 import RPi.GPIO as GPIO
 import argparse
+import signal
+import sys
 
 # constants
 MOTOR1_A = 40
@@ -11,6 +13,36 @@ MOTOR1_ENB = 10
 MOTOR2_ENA = 16
 MOTOR2_ENB = 18
 
+GPIO.setwarnings(False)
+
+motor1_ticks = 0
+motor2_ticks = 0
+
+def motor1_encoder_callback(channel):
+    global motor1_ticks
+    en_b = GPIO.input(MOTOR1_ENB)
+    if en_b > 0: 
+        motor1_ticks += 1
+        #print('going clockwise')
+    else:
+        motor1_ticks -= 1
+        #print('going counter clockwise')
+    print(f'curr number of ticks for a: {motor1_ticks}')
+
+def motor2_encoder_callback(channel):
+    global motor2_ticks
+    en_b = GPIO.input(MOTOR2_ENB)
+    if en_b > 0: 
+        motor2_ticks += 1
+        #print('going clockwise')
+    else:
+        motor2_ticks -= 1
+        #print('going counter clockwise')
+    print(f'curr number of ticks for b: {motor2_ticks}')
+
+def signal_handler(sig, frame):
+    GPIO.cleanup()
+    sys.exit(0)
 
 def main():
     
@@ -37,13 +69,16 @@ def main():
     GPIO.setup(MOTOR2_ENA, GPIO.IN)
     GPIO.setup(MOTOR2_ENB, GPIO.IN)
     
-    GPIO.output(MOTOR1_A, GPIO.HIGH)
-    GPIO.output(MOTOR1_B, GPIO.LOW)
-    GPIO.output(MOTOR2_A, GPIO.HIGH)
-    GPIO.output(MOTOR2_B, GPIO.LOW)
+   # GPIO.output(MOTOR1_A, GPIO.HIGH)
+   # GPIO.output(MOTOR1_B, GPIO.LOW)
+   # GPIO.output(MOTOR2_A, GPIO.HIGH)
+   # GPIO.output(MOTOR2_B, GPIO.LOW)
 
-    while True:
-        print(f'encoder state: {GPIO.input(MOTOR1_ENA)}, {GPIO.input(MOTOR2_ENA)}')
+    GPIO.add_event_detect(MOTOR1_ENA, GPIO.RISING, callback=motor1_encoder_callback)
+    GPIO.add_event_detect(MOTOR2_ENA, GPIO.RISING, callback=motor2_encoder_callback)
+
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.pause()
 
 if __name__ == '__main__':
     main()
