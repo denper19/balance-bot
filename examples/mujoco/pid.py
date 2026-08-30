297f7f40af344b50a84caefb1ca79b065343dd77
jermito24 <jermito24@gmail.com>
2026-08-30 15:51:33 -0700

    bad p controller

--- examples/mujoco/pid.py
+++ examples/mujoco/pid.py
@@ -1,10 +1,9 @@
 import mujoco
+import mediapy as media
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-RAD_TO_DEG = 180.0 / 3.141592653589793
-
-KP = 1.0
+KP = 20.0
 KI = 0.0
 KD = 0.0
 
@@ -24,6 +23,10 @@ def compute_control_law(tilt_speed: float) -> float:
     # derivative term
 
     control = p_term
+    if control > 0.1:
+        control = 0.1
+    elif control < -0.1:
+        control = -0.1
 
     return -control
 
@@ -33,11 +36,13 @@ xml_path_str = str(xml_path.resolve())
 model = mujoco.MjModel.from_xml_path(xml_path_str)
 data  = mujoco.MjData(model)
 
-duration = 10.0
+duration = 4
 framerate = 60
 frames = []
 tilt = []
+controls = []
 times = []
+model.opt.timestep = 0.01
 mujoco.mj_resetData(model, data)
 with mujoco.Renderer(model) as renderer:
     prev_time = 0.0
@@ -46,13 +51,25 @@ with mujoco.Renderer(model) as renderer:
         gyrodata = data.sensor('imu_gyro').data.copy()
         times.append(data.time)
         tilt_speed = gyrodata[0] # radians per second
+        if tilt_speed > 3:
+            tilt_speed = 3
+        elif tilt_speed < -3:
+            tilt_speed = -3
         tilt.append(tilt_speed)
         wheel_speed = compute_control_law(tilt_speed)
+        controls.append(wheel_speed)
         data.ctrl[0] = wheel_speed
         data.ctrl[1] = wheel_speed
         prev_time = data.time
+        with open("pid_log.txt", "a") as log_file:
+            log_file.write(f"Time: {data.time:.2f} s, Tilt Speed: {data.sensor('imu_gyro').data[0]:.4f} rad/s, Control Signal: {wheel_speed:.4f}\n")
         if len(frames) < data.time * framerate:
             renderer.update_scene(data)
             frames.append(renderer.render())
+media.write_video('pid.mp4', frames, fps=framerate)
 plt.plot(times, tilt, label='Tilt Angle')
+plt.plot(times, controls, label='Control Signal')
+plt.xlabel('Time (s)')
+plt.ylabel('Angle (rad)')
+plt.legend()
 plt.show()
