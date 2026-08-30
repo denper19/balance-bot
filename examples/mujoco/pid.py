d384657a87d2fc3b63fcd5c096411d46cd80f630
jermito24 <jermito24@gmail.com>
2026-08-30 15:13:13 -0700

    getting there

--- examples/mujoco/pid.py
+++ examples/mujoco/pid.py
@@ -2,7 +2,30 @@ import mujoco
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-TO_DEG = 180.0 / 3.141592653589793
+RAD_TO_DEG = 180.0 / 3.141592653589793
+
+KP = 1.0
+KI = 0.0
+KD = 0.0
+
+def compute_control_law(tilt_speed: float) -> float:
+
+    # tilt_speed is in radians per second
+    # we need to convert it to meters per second for the wheel speed
+    # one revolution is 6.28 radians, and the wheel circumference is 0.05 meters
+    # so radians per second * (0.05 / 6.28) = meters per second
+    error = tilt_speed * (0.05 / 6.28)
+
+    # proportional term
+    p_term = KP * error
+
+    # integral term
+
+    # derivative term
+
+    control = p_term
+
+    return -control
 
 xml_path = Path("examples/mujoco/simplified_balance_bot.xml")
 xml_path_str = str(xml_path.resolve())
@@ -13,24 +36,23 @@ data  = mujoco.MjData(model)
 duration = 10.0
 framerate = 60
 frames = []
-gyrodata = []
-acceldata = []
 tilt = []
 times = []
 mujoco.mj_resetData(model, data)
 with mujoco.Renderer(model) as renderer:
+    prev_time = 0.0
     while data.time < duration:
         mujoco.mj_step(model, data)
-        gyrodata.append(data.sensor('imu_gyro').data.copy())
-        acceldata.append(data.sensor('imu_acc').data.copy())
+        gyrodata = data.sensor('imu_gyro').data.copy()
         times.append(data.time)
+        tilt_speed = gyrodata[0] # radians per second
+        tilt.append(tilt_speed)
+        wheel_speed = compute_control_law(tilt_speed)
+        data.ctrl[0] = wheel_speed
+        data.ctrl[1] = wheel_speed
+        prev_time = data.time
         if len(frames) < data.time * framerate:
             renderer.update_scene(data)
             frames.append(renderer.render())
-prev_time = times[0]
-for i, g in enumerate(gyrodata):
-    tilt_angle = g[0] * (times[i] - prev_time) * TO_DEG
-    tilt.append(tilt_angle)
-    prev_time = times[i]
 plt.plot(times, tilt, label='Tilt Angle')
 plt.show()
