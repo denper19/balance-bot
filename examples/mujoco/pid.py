5a3b1c448dca9b862cf0ff07712a26e13a6e02e8
jermito24 <jermito24@gmail.com>
2026-08-30 01:57:11 -0700

    figuring out pid stuff

---
+++ examples/mujoco/pid.py
@@ -0,0 +1,43 @@
+import mujoco
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+TO_DEG = 180.0 / 3.141592653589793
+
+xml_path = Path("examples/mujoco/simplified_balance_bot.xml")
+xml_path_str = str(xml_path.resolve())
+
+model = mujoco.MjModel.from_xml_path(xml_path_str)
+data  = mujoco.MjData(model)
+
+duration = 10.0
+framerate = 60
+frames = []
+gyrodata = []
+acceldata = []
+tilt = []
+times = []
+mujoco.mj_resetData(model, data)
+with mujoco.Renderer(model) as renderer:
+    while data.time < duration:
+        mujoco.mj_step(model, data)
+        gyrodata.append(data.sensor('imu_gyro').data.copy())
+        acceldata.append(data.sensor('imu_acc').data.copy())
+        times.append(data.time)
+        if len(frames) < data.time * framerate:
+            renderer.update_scene(data)
+            frames.append(renderer.render())
+print(model.opt.gravity)    
+#plt.plot(times, [a[0] for a in acceldata], label='Accel X')
+#plt.plot(times, [a[1] for a in acceldata], label='Accel Y')
+#plt.plot(times, [a[2] for a in acceldata], label='Accel Z')
+prev_time = times[0]
+for i, g in enumerate(gyrodata):
+    tilt_angle = g[0] * (times[i] - prev_time) * TO_DEG
+    tilt.append(tilt_angle)
+    prev_time = times[i]
+plt.plot(times, tilt, label='Tilt Angle')
+#plt.plot(times, [g[0] for g in gyrodata], label='Gyro X')
+#plt.plot(times, [g[1] for g in gyrodata], label='Gyro Y')
+#plt.plot(times, [g[2] for g in gyrodata], label='Gyro Z')
+plt.show()
