ee58d81556efb762e01897d021b07bd47f93a87d
jermito24 <jermito24@gmail.com>
2026-08-30 20:10:11 -0700

    working now

--- examples/mujoco/pid.py
+++ examples/mujoco/pid.py
@@ -3,32 +3,28 @@ import mediapy as media
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-KP = 20.0
+KP = 1.0
 KI = 0.0
-KD = 0.0
+KD = 0.1
 
-def compute_control_law(tilt_speed: float) -> float:
+def compute_control_law(pitch_rate: float, pitch: float) -> float:
 
-    # tilt_speed is in radians per second
-    # we need to convert it to meters per second for the wheel speed
-    # one revolution is 6.28 radians, and the wheel circumference is 0.05 meters
-    # so radians per second * (0.05 / 6.28) = meters per second
-    error = tilt_speed * (0.05 / 6.28)
 
     # proportional term
-    p_term = KP * error
+    p_term = KP * pitch
 
     # integral term
 
     # derivative term
+    d_term = KD * pitch_rate
 
-    control = p_term
-    if control > 0.1:
-        control = 0.1
-    elif control < -0.1:
-        control = -0.1
+    control = p_term + d_term
+#   if control > 10:
+#       control = 10
+#   elif control < -10:
+#       control = -10
 
-    return -control
+    return control
 
 xml_path = Path("examples/mujoco/simplified_balance_bot.xml")
 xml_path_str = str(xml_path.resolve())
@@ -42,7 +38,8 @@ frames = []
 tilt = []
 controls = []
 times = []
-model.opt.timestep = 0.01
+pitch_angle = 0.0
+model.opt.timestep = 0.002
 mujoco.mj_resetData(model, data)
 with mujoco.Renderer(model) as renderer:
     prev_time = 0.0
@@ -51,12 +48,9 @@ with mujoco.Renderer(model) as renderer:
         gyrodata = data.sensor('imu_gyro').data.copy()
         times.append(data.time)
         tilt_speed = gyrodata[0] # radians per second
-        if tilt_speed > 3:
-            tilt_speed = 3
-        elif tilt_speed < -3:
-            tilt_speed = -3
-        tilt.append(tilt_speed)
-        wheel_speed = compute_control_law(tilt_speed)
+        pitch_angle += tilt_speed * (data.time - prev_time) # integrate to get angle
+        tilt.append(pitch_angle)
+        wheel_speed = compute_control_law(gyrodata[0], pitch_angle)
         controls.append(wheel_speed)
         data.ctrl[0] = wheel_speed
         data.ctrl[1] = wheel_speed
